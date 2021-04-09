8bf4ee65dc104bd0ac2b0f2a53978933e5921ed5
Kushank Sriraj <kushanksriraj@gmail.com>
2021-04-09 09:48:59 +0530

    style: desktop view and responsive

--- src/data.js
+++ src/data.js
@@ -6,10 +6,10 @@ export const videoList = [
       "Red Bull invited Tom Cruise to take a break from production of his new movie, Mission: Impossible Ghost Protocol, to pilot the Red Bull Racing F1 car through Southern California's 2.5-mile (4km) Willow Springs circuit."
   },
   {
-    id: "epdmatdvSzk",
-    name: "F1 2021 Rules: Everything You Need To Know",
+    id: "fS8Ezkxwn5g",
+    name: "The Rules of Formula One 1 - F1 - EXPLAINED!",
     desc:
-      "Red Bull invited Tom Cruise to take a break from production of his new movie, Mission: Impossible Ghost Protocol, to pilot the Red Bull Racing F1 car through Southern California's 2.5-mile (4km) Willow Springs circuit."
+      "Ninh explains - The Rules of Formula One (F1). The most popular form of single seat auto car racing in the world. Watch this short beginner’s tutorial video guide on how Formula 1 is raced, F1 rules, and how to to understand the races at a Formula One Grand Prix"
   },
   {
     id: "6OLVFa8YRfM",
@@ -24,22 +24,22 @@ export const videoList = [
       "Red Bull invited Tom Cruise to take a break from production of his new movie, Mission: Impossible Ghost Protocol, to pilot the Red Bull Racing F1 car through Southern California's 2.5-mile (4km) Willow Springs circuit."
   },
   {
-    id: "QuBlV0dixMo",
-    name: "Race Highlights | 2021 Bahrain Grand Prix",
+    id: "g-NUz0pQC0I",
+    name: "Formula 1 For New Fans: The History Of F1 & Everything A New Fan Needs To Know EXPLAINED",
     desc:
-      "Red Bull invited Tom Cruise to take a break from production of his new movie, Mission: Impossible Ghost Protocol, to pilot the Red Bull Racing F1 car through Southern California's 2.5-mile (4km) Willow Springs circuit."
+      "This is SPECIFICALLY designed for new Formula 1 fans who feel like there isn't a comprehensive explanation of F1 out there. I'll take you through a brief history and touch on important, and often un-explained topics. "
   },
   {
-    id: "HF9VoGwENNg",
-    name: "Top 10 Moments of Pre-Race Chaos",
+    id: "L6AeUB8mbc8",
+    name: "F1 2020 Beginners Guide (Updated Version)",
     desc:
-      "Red Bull invited Tom Cruise to take a break from production of his new movie, Mission: Impossible Ghost Protocol, to pilot the Red Bull Racing F1 car through Southern California's 2.5-mile (4km) Willow Springs circuit."
+      "Wether you are an F1 noob or just looking to learn more about the sport this F1 2020 Beginners Guide is just for you. "
   },
   {
-    id: "rnIjQC08qKk",
-    name: "Top 10 Cheeky F1 Innovations",
+    id: "gguC0RQxYUA",
+    name: "How much do you know about F1? | ABC of Formula 1 Part 1",
     desc:
-      "Red Bull invited Tom Cruise to take a break from production of his new movie, Mission: Impossible Ghost Protocol, to pilot the Red Bull Racing F1 car through Southern California's 2.5-mile (4km) Willow Springs circuit."
+      "Find out everything you need to know about the international motorsport of Formula One racing with facts and figures, as well as lessons in the terminology, disciplines, history, heroes, and drama of the sport."
   },
   {
     id: "awcfwNrmm8A",
@@ -48,15 +48,15 @@ export const videoList = [
       "Red Bull invited Tom Cruise to take a break from production of his new movie, Mission: Impossible Ghost Protocol, to pilot the Red Bull Racing F1 car through Southern California's 2.5-mile (4km) Willow Springs circuit."
   },
   {
-    id: "0lj6Q9gN4RQ",
-    name: "2018 Monaco Grand Prix: Race Highlights",
+    id: "4E1K6STJwus",
+    name: "Drive a lap with the MP4/4 and Fernando Alonso",
     desc:
-      "Red Bull invited Tom Cruise to take a break from production of his new movie, Mission: Impossible Ghost Protocol, to pilot the Red Bull Racing F1 car through Southern California's 2.5-mile (4km) Willow Springs circuit."
+      "Possibly the most epic install lap of all time. Drive a lap of the Circuit de Catalunya with Fernando Alonso at the wheel of Ayrton Senna's iconic MP4/4."
   },
   {
-    id: "icRDb5mV2SA",
-    name: "Fernando Alonso's Greatest Renault Moments",
+    id: "VEJh4lLCzRc",
+    name: "Racing Lines explained",
     desc:
-      "Red Bull invited Tom Cruise to take a break from production of his new movie, Mission: Impossible Ghost Protocol, to pilot the Red Bull Racing F1 car through Southern California's 2.5-mile (4km) Willow Springs circuit."
+      "What are racing lines and how do they make you faster? Why is the racing line not as simple as the shortest, fastest or easiest path through a turn? What is the karting line? What does it mean to take a late apex? Let's explore!"
   }
 ];
